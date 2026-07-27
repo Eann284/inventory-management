ea4bf396bcb916590e46582ba4dfb22f78b18721
Eann <eannsethparreno28@gmail.com>
2026-07-27 19:22:59 +0800

    Created models, connected DB to backend, Created Superuser (warehouseadmin@gmail.com)

--- backend/stock/models.py
+++ backend/stock/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from users.models import UserManagerModel
+from wh_admin_logs.models import WarehouseAdminLogs
+from products.models import Product
 
 # Create your models here.
+
+
+class AddStock(models.Model):
+    add_id = models.CharField(max_length=50, primary_key=True)
+
+    wh_id = models.ForeignKey(
+        UserManagerModel,
+        on_delete=models.CASCADE,
+        db_column='wh_id',
+        to_field='uid',
+        related_name='add_stock'
+        
+    )
+
+    wh_logs = models.ForeignKey(
+        WarehouseAdminLogs,
+        on_delete=models.CASCADE,
+        db_column='wh_logs',
+    )
+
+    date = models.DateTimeField(auto_now_add=True)
+
+    product_id = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        db_column='product_id'
+    )
+
+    quantity = models.IntegerField()
+
+    class Meta:
+        db_table = 'add_stock'
+        unique_together = [['wh_id','wh_logs']]
--- backend/wh_admin_logs/models.py
+++ backend/wh_admin_logs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import UserManagerModel
 
 # Create your models here.
+
+class WarehouseAdminLogs(models.Model):
+    wh_id = models.ForeignKey(
+        UserManagerModel,
+        on_delete=models.CASCADE,
+        db_column='wh_id',
+        to_field='uid'
+    )
+    wh_logs = models.CharField(max_length=100, primary_key=True)
+    date = models.DateTimeField(auto_now_add=True)
+    wh_proof = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.wh_id.name} - {self.wh_proof}"
+
+    class Meta:
+        db_table = 'wh_admin_logs'
