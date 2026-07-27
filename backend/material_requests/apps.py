0f1e15bebc946312cc39f221e18192d0fd5e8f17
Eann <eannsethparreno28@gmail.com>
2026-07-27 17:13:50 +0800

    Project Setup and Settings

---
+++ backend/material_requests/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MaterialRequestsConfig(AppConfig):
+    name = 'material_requests'
---
+++ backend/wh_admin_logs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WhAdminLogsConfig(AppConfig):
+    name = 'wh_admin_logs'
